7a57ed9629fdfee567e3226db3a3b73a8dfc17a3
shershah <i200523@nu.edu.pk>
2022-09-02 02:44:21 +0500

    final

--- index.js
+++ index.js
@@ -26,6 +26,22 @@ function result(num){
     
 }
 
+function calculateFactorial(num){
+  let fact = 1;
+  for (let index = 1; index <= num; index++) {
+          fact *= index;
+  }
+  return fact;
+}
+
+function nCr(n, r){
+    return calculateFactorial(n) / (calculateFactorial(r) * calculateFactorial(n - r));
+}
+
+function nPr(n, r){
+    return Math.floor(calculateFactorial(n) / calculateFactorial(n - r));
+}
+
 
 function calculate(value){
     if (input !==0) {
@@ -35,27 +51,40 @@ function calculate(value){
 
     var answer = value[0];
     var dividedByZero = 0;
-    for (var i = 2; i < value.length; i= i+2) {
-        switch (queue[i-1]) {
-            case '+':
-                answer+= value[i];
-                break;
-            case '-':
-                answer-= value[i];
-                break;
-            case '/':    
-                if (value[i] === 0)   
-                  dividedByZero = 1;
-                else      
-                  answer = answer / value[i];
-                break;
-            case'*': 
-                  answer = answer * value[i];
-                break;
-            case'%':
-                  answer = answer % value[i];
-        }
+
+    if(value.length == 2){
+      answer = calculateFactorial(answer)
     }
+    else{
+      for (var i = 2; i < value.length; i= i+2) {
+          switch (queue[i-1]) {
+              case '+':
+                  answer+= value[i];
+                  break;
+              case '-':
+                  answer-= value[i];
+                  break;
+              case '/':    
+                  if (value[i] === 0)   
+                    dividedByZero = 1;
+                  else      
+                  answer = answer / value[i];
+                  break;
+              case'*': 
+                  answer = answer * value[i];
+                  break;
+              case'%':
+                  answer = answer % value[i];
+                  break;
+              case 'C':
+                  answer = nCr(answer,value[i]);
+                  break;
+              case 'P':
+                  answer = nPr(answer,value[i]);
+                  break;
+          }
+      }
+  }
     
     answer = answer.toFixed(5);
     answer = parseFloat(answer);
@@ -72,14 +101,8 @@ function calculate(value){
 }
 
 function numeric(arg){
-  if ( calcDisplay.innerHTML ===  "ERROR" || (calcDisplay.innerHTML == "0" && arg != ".")){ 
-      calcDisplay.innerHTML = ""; 
-  }
-
-  if (!(arg === ".") || !input.match(/[.]/)) {
     input += arg;
     calcDisplay.innerHTML += arg;
-  }
 }
 
 function operator(arg){
@@ -91,130 +114,9 @@ function operator(arg){
       input = 0;
   }
   
-  if (arg == "-"  && isNaN(queue[0]) && input !== "-"){
+  // checks if first input is the negative sign, doesnot allow 2 consective negative signs
+  if (arg == "-"  && isNaN(queue[0]) && input != "-"){
       input ="-";
       calcDisplay.innerHTML = "-";
   }
 }
-
-
-
-
-// var input = document.getElementById('input'), // input/output button
-//   number = document.querySelectorAll('.numbers div'), // number buttons
-//   operator = document.querySelectorAll('.operators div'), // operator buttons
-//   result = document.getElementById('result'), // equal button
-//   clear = document.getElementById('clear'), // clear button
-//   resultDisplayed = false; // flag to keep an eye on what output is displayed
-
-// // adding click handlers to number buttons
-// for (var i = 0; i < number.length; i++) {
-//   number[i].addEventListener("click", function(e) {
-
-//     // storing current input string and its last character in variables - used later
-//     var currentString = input.innerHTML;
-//     var lastChar = currentString[currentString.length - 1];
-
-//     // if result is not diplayed, just keep adding
-//     if (resultDisplayed === false) {
-//       input.innerHTML += e.target.innerHTML;
-//     } else if (resultDisplayed === true && lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
-//       // if result is currently displayed and user pressed an operator
-//       // we need to keep on adding to the string for next operation
-//       resultDisplayed = false;
-//       input.innerHTML += e.target.innerHTML;
-//     } else {
-//       // if result is currently displayed and user pressed a number
-//       // we need clear the input string and add the new input to start the new opration
-//       resultDisplayed = false;
-//       input.innerHTML = "";
-//       input.innerHTML += e.target.innerHTML;
-//     }
-
-//   });
-// }
-
-// // adding click handlers to number buttons
-// for (var i = 0; i < operator.length; i++) {
-//   operator[i].addEventListener("click", function(e) {
-
-//     // storing current input string and its last character in variables - used later
-//     var currentString = input.innerHTML;
-//     var lastChar = currentString[currentString.length - 1];
-
-//     // if last character entered is an operator, replace it with the currently pressed one
-//     if (lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
-//       var newString = currentString.substring(0, currentString.length - 1) + e.target.innerHTML;
-//       input.innerHTML = newString;
-//     } else if (currentString.length == 0) {
-//       // if first key pressed is an opearator, don't do anything
-//       console.log("enter a number first");
-//     } else {
-//       // else just add the operator pressed to the input
-//       input.innerHTML += e.target.innerHTML;
-//     }
-
-//   });
-// }
-
-// // on click of 'equal' button
-// result.addEventListener("click", function() {
-
-//   // this is the string that we will be processing eg. -10+26+33-56*34/23
-//   var inputString = input.innerHTML;
-
-//   // forming an array of numbers. eg for above string it will be: numbers = ["10", "26", "33", "56", "34", "23"]
-//   var numbers = inputString.split(/\+|\-|\×|\÷/g);
-
-//   // forming an array of operators. for above string it will be: operators = ["+", "+", "-", "*", "/"]
-//   // first we replace all the numbers and dot with empty string and then split
-//   var operators = inputString.replace(/[0-9]|\./g, "").split("");
-
-//   console.log(inputString);
-//   console.log(operators);
-//   console.log(numbers);
-//   console.log("----------------------------");
-
-//   // now we are looping through the array and doing one operation at a time.
-//   // first divide, then multiply, then subtraction and then addition
-//   // as we move we are alterning the original numbers and operators array
-//   // the final element remaining in the array will be the output
-
-//   var divide = operators.indexOf("÷");
-//   while (divide != -1) {
-//     numbers.splice(divide, 2, numbers[divide] / numbers[divide + 1]);
-//     operators.splice(divide, 1);
-//     divide = operators.indexOf("÷");
-//   }
-
-//   var multiply = operators.indexOf("×");
-//   while (multiply != -1) {
-//     numbers.splice(multiply, 2, numbers[multiply] * numbers[multiply + 1]);
-//     operators.splice(multiply, 1);
-//     multiply = operators.indexOf("×");
-//   }
-
-//   var subtract = operators.indexOf("-");
-//   while (subtract != -1) {
-//     numbers.splice(subtract, 2, numbers[subtract] - numbers[subtract + 1]);
-//     operators.splice(subtract, 1);
-//     subtract = operators.indexOf("-");
-//   }
-
-//   var add = operators.indexOf("+");
-//   while (add != -1) {
-//     // using parseFloat is necessary, otherwise it will result in string concatenation :)
-//     numbers.splice(add, 2, parseFloat(numbers[add]) + parseFloat(numbers[add + 1]));
-//     operators.splice(add, 1);
-//     add = operators.indexOf("+");
-//   }
-
-//   input.innerHTML = numbers[0]; // displaying the output
-
-//   resultDisplayed = true; // turning flag if result is displayed
-// });
-
-// // clearing the input on press of clear
-// clear.addEventListener("click", function() {
-//   input.innerHTML = "";
-// })
